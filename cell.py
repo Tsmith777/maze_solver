b53b65a10b1453c167dec129a7f73225e179accd
Tsmith777 <Tsmith777@ymail.com>
2025-05-04 09:58:55 -0700

    Added: cell.py

--- cell.py
+++ cell.py
@@ -3,7 +3,7 @@ from graphics import Line, Point
 class Cell:
     def __init__(self, win):
         self.has_left_wall = True
-        self.has.right_wall = True
+        self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
         self._x1 = None
